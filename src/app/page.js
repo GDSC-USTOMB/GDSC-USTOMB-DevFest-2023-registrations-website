651bc5e4fca1b5ef5fca1d47a2ff3901edd9f4e0
emerias <m.zakariahamoudi@gmail.com>
2023-12-08 15:10:53 +0100

    second prototype

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,52 @@
 import Image from "next/image";
-import Form from "../components/form"
-import Footer from "../components/footer"
+import Form from "../components/form";
+import Footer from "../components/footer";
 import { Trykker } from "next/font/google";
 import banner from "../../public/banner.png";
 import bracket1 from "../../public/bracket1.png";
 import bracket2 from "../../public/bracket2.png";
 
 export default function Home() {
-  
   return (
     <div className=" h-screen ">
+      <div className="banner">
       <Image
         src={banner}
-        className=" w-screen lg:h-[300px] h-[150px] object-cover"
+        className="lg:h-[300px] h-[150px] mx-auto"
+        alt="banner"
       />
+      </div>
+    
+    
       <br />
-      <div className="lg:px-[250px]">
-        <div className="flex justify-evenly">
-          <Image className="w-[75px] h-[75px] lg:w-[200px] lg:h-[200px] " src={bracket1} />
-          <div>
-             <p className="text-center lg:text-[31px]">
-            Welcome to <span className="font-bold ">DevFest Oran 2023</span>.
-            <br />
-            Hosted by <span className="font-bold ">GDSC USTO!</span>
-          </p>
-          <div className="flex gap-[20px] lg:mt-[30px] mt-[10px]  text-center justify-center   ">
-            <p className="lg:text-[32px] font-semibold  bg-[#D9D9D9] lg:px-[80px] px-[20px] rounded-lg text-center">
-              16 Dec 2023
+      <div className="lg:px-[150px]">
+        <div className="flex justify-center lg:gap-[80px] gap-[20px] ">
+          <Image
+            className="w-[75px] h-[75px] lg:w-[200px] lg:h-[200px] "
+            src={bracket1}
+            alt="bracket1"
+          />
+          <div className="">
+            <p className="text-center lg:text-[31px]">
+              Welcome to <span className="font-bold ">DevFest Oran 2023</span>.
+              <br />
+              Hosted by <span className="font-bold ">GDSC USTO!</span>
             </p>
-            <p className="lg:text-[32px] font-semibold  bg-[#D9D9D9] lg:px-[80px] px-[20px] rounded-lg text-center">
-              9:30 AM
-            </p>
+            <div className="flex gap-[20px] lg:mt-[30px] mt-[10px]  text-center justify-center   ">
+              <p className="lg:text-[26px] font-semibold  bg-[#D9D9D9] lg:w-[200px] px-[20px] rounded-lg text-center">
+                16 Dec 2023
+              </p>
+              <p className="lg:text-[26px] font-semibold  bg-[#D9D9D9] lg:w-[200px] px-[20px] rounded-lg text-center">
+                9:30 AM
+              </p>
+            </div>
           </div>
-          </div>
-         
-          <Image className="w-[75px] h-[75px] lg:w-[200px] lg:h-[200px] " src={bracket2} />
+
+          <Image
+            className="w-[75px] h-[75px] lg:w-[200px] lg:h-[200px] "
+            src={bracket2}
+            alt="bracket2"
+          />
         </div>
 
         <p className="lg:text-[27px] px-[10px] mt-[20px] text-center">
@@ -44,12 +56,13 @@ export default function Home() {
           this exciting event.
         </p>
       </div>
-      <br/>
-      <br/>
-      <Form/>
-      <br/>
-      <br/>
-      <Footer/>
+      <br />
+      <br />
+      <h1 className='text-center  lg:text-4xl font-bold text-[#fff] relative top-[50px] z-[50]  lg:w-[400px] w-[200px]  py-[30px] rounded-lg mx-auto bg-[#1B1942]'>Registration Form</h1>
+      <Form />
+      <br />
+      <br />
+      <Footer />
     </div>
   );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import mongoose from "mongoose";
+import Swal from 'sweetalert2'
 import Registration from "../models/User";
 const form = () => {
+
+
+
     async function newRegistration(data) {
         "use server";
         let fullname = data.get("fullname").valueOf();
@@ -19,23 +23,31 @@ const form = () => {
           let newRegistration = new Registration({ fullname, email, phone, univ_comp, field, position, questions });
           await newRegistration.save();
           console.log(newRegistration);
+          sucessMessage();
+          
         } catch (error) {
           console.log(error);
         }
       }
   return (
-    <div class=" relative bg-[#ECECF4] mt-[50px] lg:px-[250px] px-[30px]">
-        <form className='bg-[#e2e1f3] shadow-lg rounded-lg' action={newRegistration}>
+    <div className=" relative bg-[#ECECF4]  lg:px-[250px] px-[30px]">
+        <form className='bg-[#e2e1f3]  shadow-lg rounded-lg' action={newRegistration}>
         <div className="px-[30px] py-[75px] flex flex-col gap-[5px]">
-       
-        <input type="text" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="FullName *" name="fullname" id="fullname" /><br/>
-        <input type="email" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Email Adress *" name="email" id="email" /><br/>
-        <input type="phone" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Phone Number *" name="phone" id="phone" /><br/>
-        <input type="text" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="University / Company *" name="univ/comp" id="univ_comp" /><br/>
-        <input type="text" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Field of Study (For Students ) *" name="field" id="field" /><br/>
-        <input type="text" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Position (For Professionals ) *" name="position" id="position" /><br/>
-        <input type="text" className="placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Any specific questions or comments *" name="questions" id="questions" /><br/>
-        <input value="Register" className="font-semibold text-xl h-[50px] w-[250px] mx-auto rounded-lg text-[#fff] bg-[#1B1942]" type="submit"  />
+       <label className='text-xl' for="fullname">Fullname :</label>
+        <input type="text" className="ml-[5px] placeholder-[#000000] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="FullName *" name="fullname" id="fullname" /><br/>
+        <label className='text-xl' for="email">Email :</label>
+        <input type="email" className="placeholder-[#000000] ml-[5px] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Email Adress *" name="email" id="email" /><br/>
+        <label className='text-xl' for="phone">Phone Number :</label>
+        <input type="phone" className="placeholder-[#000000] ml-[5px] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Phone Number *" name="phone" id="phone" /><br/>
+        <label className='text-xl' for="univ/comp">University / Company :</label>
+        <input type="text" className="placeholder-[#000000] ml-[5px] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="University / Company *" name="univ/comp" id="univ_comp" /><br/>
+        <label className='text-xl' for="field">Field of Study :</label>
+        <input type="text" className="placeholder-[#000000] ml-[5px] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Field of Study (For Students ) *" name="field" id="field" /><br/>
+        <label className='text-xl' for="position">Position :</label>
+        <input type="text" className="placeholder-[#000000] ml-[5px] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Position (For Professionals ) *" name="position" id="position" /><br/>
+        <label className='text-xl' for="questions">Questions :</label>
+        <input type="text" className="placeholder-[#000000] ml-[5px] border-[#07313F] border-2 w-full h-[60px] pl-[15px] rounded-md" placeholder="Any specific questions or comments *" name="questions" id="questions" /><br/>
+        <button   className="font-semibold text-xl h-[50px] w-[250px] mx-auto rounded-lg text-[#fff] bg-[#1B1942]   hover:bg-[#242258] duration-150" type="submit">Register</button>
         </div>
       </form>
     </div>
